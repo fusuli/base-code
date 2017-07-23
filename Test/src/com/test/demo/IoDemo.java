b2bd81a625efd099c92f002e4cbd8d4465425078
fusuli <15551103598@163.com>
2017-07-23 09:58:29 +0800

    <案例><新建立><IO 练习案例>

---
+++ Test/src/com/test/demo/IoDemo.java
@@ -0,0 +1,15 @@
+package com.test.demo;
+
+/**
+ * IO Á·Ï°°¸Àý
+ * @author fusu
+ *
+ */
+public class IoDemo {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
