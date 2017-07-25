20ada5c560faf181a58ca7ee5f27b4a781579cfe
fusuli <15551103598@163.com>
2017-07-25 18:58:21 +0800

    <Java继承>

---
+++ Test/src/com/test/demo/Animal.java
@@ -0,0 +1,11 @@
+package com.test.demo;
+
+public class Animal {
+	public int age;
+	public String name;
+	public void eat() {
+		System.out.println("动物会吃东西");
+	}
+	
+
+}
---
+++ Test/src/com/test/demo/Initail.java
@@ -0,0 +1,13 @@
+package com.test.demo;
+
+public class Initail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog d=new Dog();
+		d.age=10;
+		d.name="xiaobai";
+		d.eat();
+	}
+
+}
