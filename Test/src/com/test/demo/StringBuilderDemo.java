2a7c05c1f358c7dff0d2dc6a91e54ba0eacb2be4
fusuli <15551103598@163.com>
2017-07-27 19:00:22 +0800

    <StringBuilder没有实现线程安全功能，性能略高>

---
+++ Test/src/com/test/demo/StringBuilderDemo.java
@@ -0,0 +1,12 @@
+package com.test.demo;
+
+public class StringBuilderDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//创建一个StringBuilder对象，用来存储字符串
+		StringBuilder hobby=new StringBuilder("爱生活，爱自己");
+		System.out.println(hobby);
+	}
+
+}
