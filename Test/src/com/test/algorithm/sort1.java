bf393b847b8dca913bf71e5d213287a3228552a1
fusuli <15551103598@163.com>
2017-07-29 22:12:47 +0800

    <调用静态Arrays.sort()方法来排序>

---
+++ Test/src/com/test/algorithm/sort1.java
@@ -0,0 +1,16 @@
+package com.test.algorithm;
+
+import java.util.Arrays;
+
+public class sort1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = new int[] { 3, 4, 2, 5, 6, 7, 8, 9 };
+		Arrays.sort(a);
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+}
