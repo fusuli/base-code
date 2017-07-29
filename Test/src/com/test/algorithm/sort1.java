fd5501215696d0644681ec1320e56b9857906b4a
fusuli <15551103598@163.com>
2017-07-29 22:22:06 +0800

    <java基础算法：字符串排序>

--- Test/src/com/test/algorithm/sort1.java
+++ Test/src/com/test/algorithm/sort1.java
@@ -6,7 +6,7 @@ public class sort1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = new int[] { 3, 4, 2, 5, 6, 7, 8, 9 };
+		String[] a =new String[]{"o","u","d","c","b","a"};
 		Arrays.sort(a);
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
